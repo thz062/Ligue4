c3187ebda54642ea40c1f79c58c9ccdebdf7e84a
thz062 <thialbuquerquest@gmail.com>
2026-02-02 14:27:32 -0300

    Refactor main function and add game initialization

--- Ligue4++.c
+++ Ligue4++.c
@@ -1,10 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+    
+typedef struct {
+    char simbolo; 
+    int jogador; 
+} Posicao;
 
-int main()
-{
+void iniciarTabuleiro(Posicao tabuleiro[6][7]){
+    for (int i=0; i<6; i++){
+        for (int j=0; j<7; j++){
+            tabuleiro[i][j].simbolo = ' ';
+            tabuleiro[i][j].jogador = 0;
+    
+        }
+    }
+}
+void imprimitTabuleiro(Posicao tabuleiro[6][6]){
+    printf("\n");
+    for (int i = 0; i < 6; i++) {
+        printf("|");
+        for (int j = 0; j < 7; j++) {
+            printf(" %c |", tabuleiro[i][j].simbolo);
+        }
+        printf("\n");
+    }
+
+    printf("  1   2   3   4   5   6   7\n\n");
+}
+
+int inserirFicha(Posicao tabuleiro[6][7], int coluna, int jogador){
+    for (int i=5; i>=0; i--){
+        if (tabuleiro[i][coluna].jogador ==0 ){
+            tabuleiro[i][coluna].jogador = jogador;
+            tabuleiro[i][coluna].simbolo = (jogador == 1) ? 'X' : 'O';
+            return 1;
+        }
+    }
+    return 1;
+}
+
+
+
+
+
+
+int main(){
+
 	int opcao;
-	
+   do {
 	printf("Ligue 4++\n");
     printf("1. Inicia novo jogo\n");
     printf("2. Hall dos campeões\n");
@@ -12,7 +55,16 @@ int main()
     printf("Escolha uma opção; ");
     scanf("%d", &opcao);
 
-    switch(opcao)
+    if (opcao == 1){
+        jogar();
+    }else if (opcao == 2){
+        printf("Sair\n");
+    } else {
+        printf("Opcao invalida.\n");
+    }
+    return 0;
+   }
+    /*switch(opcao)
     {
         case 1:
             printf("Iniciando novo jogo...\n");
@@ -30,10 +82,7 @@ int main()
             printf("Opção inválida!\n");
     }
 
-    typedef struct {
-        char simbolo; 
-        int jogador; 
-    } Posicao;
+    
 
     Posicao tabuleiro[6][7];
 
@@ -46,15 +95,6 @@ int main()
 
     printf("\n");
 
-    for (int i = 0; i < 6; i++) {
-        printf("|");
-        for (int j = 0; j < 7; j++) {
-            printf(" %c |", tabuleiro[i][j].simbolo);
-        }
-        printf("\n");
-    }
-
-    printf("  1   2   3   4   5   6   7\n");
 
     int coluna;
 
@@ -79,7 +119,7 @@ int main()
 
     if(linhainserida == -1) {
         printf("Essa coluna está cheia!\n");
-        return 0;
+        
     }
 	return 0;
-}
+}*/
